10b36c33b833c42233cca5601d31c24446ee9aed
AnigalHub <anigal1997@gmail.com>
2023-09-02 15:03:21 +0300

    добавлен метод выбора документа, карусель на маленьких экранах

--- src/components/Customs.vue
+++ src/components/Customs.vue
@@ -4,12 +4,21 @@
             <div class="title">
                 <h2><span>Официальный таможенный представитель</span></h2>
             </div>
-            <b-row>
-                <b-col v-for="value in Customs" :key="value.index">
-                    <img :src="value.src" :alt="value.alt">
-                    <component :is="svg"/>
-                </b-col>
-            </b-row>
+            <div class="flex-container">
+                <div v-for="value in Customs" :key="value.index">
+                    <img :src="value.src" :alt="value.alt" @click="showModal(value)">
+                </div>
+            </div>
+            <b-carousel id="carousel-fade" fade indicators>
+                <b-carousel-slide v-for="value in Customs">
+                    <template v-slot:img>
+                        <img :src="value.src" :alt="value.alt" @click="showModal(value)"/>
+                    </template>
+                </b-carousel-slide>
+            </b-carousel>
+            <b-modal ref="my-modal" id="img" size="lg" centered :title="selectedImages.name">
+                <img :src="selectedImages.src" :alt="selectedImages.alt"/>
+            </b-modal>
             <p>Мы работаем честно и прозрачно: каждый шаг подтверждается документами, каждое действие соответствует законам КНР или РФ</p>
         </b-container>
     </div>
@@ -22,6 +31,10 @@
         data(){
             return{
                 svg:increase,
+                selectedImages:{
+                    src:'',
+                    alt:'',
+                },
                 Customs: [
                     {
                         src:'./customs/1.png',
@@ -37,6 +50,12 @@
                     }
                 ],
             }
+        },
+        methods: {
+            showModal(img) {
+                this.selectedImages = img;
+                this.$refs['my-modal'].show()
+            },
         }
     }
 </script>
