81e0f947a8367f367b892c7088bbf48c97e7124c
AnigalHub <anigal1997@gmail.com>
2023-09-04 16:25:18 +0300

    изменена форма ввода: добавлены v-model, b-form-checkbox

--- src/components/Calculation.vue
+++ src/components/Calculation.vue
@@ -2,46 +2,18 @@
     <div id="Calculation">
         <b-container>
             <h2>Расчет стоимости доставки груза</h2>
-            <div class="flex-container">
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="Пункт отправления"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="Пункт назначения"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="Наименование"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="ФИО"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="+7(9**)***-**-**"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="Email"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="number" placeholder="Объем груза, м3"/>
-                </div>
-                <div>
-                    <b-form-input autocomplete="off" type="text" placeholder="Вес груза, кг"/>
-                </div>
-                <div>
-                    <p>Страховка</p>
-                    <label class="switch">
-                        <input type="checkbox">
-                        <span class="slider round"></span>
-                    </label>
-                </div>
-                <div>
-                    <p>Упаковка</p>
-                    <label class="switch">
-                        <input type="checkbox">
-                        <span class="slider round"></span>
-                    </label>
-                </div>
-            </div>
+            <form class="flex-container">
+                <div><b-form-input autocomplete="off" v-model="formCalculation.departure" type="text" placeholder="Пункт отправления"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.arrival" type="text" placeholder="Пункт назначения"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.name" type="text" placeholder="Наименование"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.fio" type="text" placeholder="ФИО"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.phone" type="tel" v-mask="'+7 (###) ###-##-##'" placeholder="+7(9**)***-**-**"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.email" type="text" placeholder="Email"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.volume" type="number" placeholder="Объем груза, м3"/></div>
+                <div><b-form-input autocomplete="off" v-model="formCalculation.weight" type="text" placeholder="Вес груза, кг"/></div>
+                <div><b-form-checkbox v-model="formCalculation.insurance" name="check-button" switch><p>Страховка</p></b-form-checkbox></div>
+                <div><b-form-checkbox v-model="formCalculation.package" name="check-button" switch><p>Упаковка</p></b-form-checkbox></div>
+            </form>
             <b-button>ОТПРАВИТЬ ЗАЯВКУ  <component :is="svg"/></b-button>
         </b-container>
     </div>
@@ -54,6 +26,18 @@
         data(){
             return{
                 svg:arrow,
+                formCalculation:{
+                    departure:'',
+                    arrival:'',
+                    name: '',
+                    fio:'',
+                    phone: '',
+                    email:'',
+                    volume:'',
+                    weight:'',
+                    insurance: false,
+                    package: false,
+                },
             }
         }
     }
